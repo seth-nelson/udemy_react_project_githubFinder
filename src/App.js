2e174b680fd32d9248af0d3336dd4601d6661404
Seth Nelson <seth.nelson@outlook.com>
2021-01-19 17:57:57 -0800

    started on nav bar and passed title props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title='Github Finder'/>
+      </div>
+    );
+  }
 }
 
 export default App;

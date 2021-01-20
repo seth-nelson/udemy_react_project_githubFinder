a65f2225de4cc63ea9fc3b6728df440bf5a90799
Seth Nelson <seth.nelson@outlook.com>
2021-01-19 18:04:02 -0800

    finished nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
+    // Defaults to this if no props are passed in
+    static defaultProps = {
+        title: 'Github Finder'
+    };
+
+    // Checks the type of your prop to ensure proper mounting
+    static propTypes = {
+        title: PropTypes.string.isRequired
+    }
+
     render() {
         return (
             <nav className='navbar bg-primary'>

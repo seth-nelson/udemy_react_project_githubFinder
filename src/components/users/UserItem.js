df0648999d656954317d66928f041c6c4965f93c
Seth Nelson <seth.nelson@outlook.com>
2021-01-21 17:06:22 -0800

    transitioned the class component to a functional component that passes props for state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
+const UserItem = (props) => {
+    const { login, avatar_url, html_url} = props.user;
 
-        render() {
-            const { login, avatar_url, html_url} = this.props.user;
+    return (
+        <div className='card text-center'>
+            <img 
+                src={avatar_url} 
+                alt='' 
+                className='round-img' 
+                style={{ width: '60px' }}
+            />
+            <h3>{login}</h3>
 
-            return (
-                <div className='card text-center'>
-                    <img 
-                        src={avatar_url} 
-                        alt='' 
-                        className='round-img' 
-                        style={{ width: '60px' }}
-                    />
-                    <h3>{login}</h3>
-
-                    <div>
-                        <a href={html_url} className='btn btn-dark btn-sm my-1'>More</a>
-                    </div>
-                </div>
-            )
-        }
+            <div>
+                <a href={html_url} className='btn btn-dark btn-sm my-1'>More</a>
+            </div>
+        </div>
+    )
 }
 
 

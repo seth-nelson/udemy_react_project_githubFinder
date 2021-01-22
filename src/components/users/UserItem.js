dda758461be2944736747ba5e615af43fb9e1793
Seth Nelson <seth.nelson@outlook.com>
2021-01-21 17:48:47 -0800

    modified the state to update based on the props given to render the axios API fetch call request

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const UserItem = ({user: { login, avatar_url, html_url } }) => {
     return (
@@ -18,5 +19,8 @@ const UserItem = ({user: { login, avatar_url, html_url } }) => {
     )
 }
 
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+};
 
 export default UserItem

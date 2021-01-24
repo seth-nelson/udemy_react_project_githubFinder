9cf84882e3858a1d61b9753651e648c7f02d0162
Seth Nelson <seth.nelson@outlook.com>
2021-01-23 19:49:22 -0800

    modified search to update state based on the text inputted on the form

--- src/App.js
+++ src/App.js
@@ -11,14 +11,24 @@ class App extends Component {
     isLoading: false
   }
 
-  async componentDidMount() {
-    this.setState({isLoading: true});
+  // --- TEST USERS ---
 
-    const res = await axios.get(`https://api.github.com/users?client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  // async componentDidMount() {
+  //   this.setState({isLoading: true});
 
-    this.setState({ users: res.data, isLoading: false})
+  //   const res = await axios.get(`https://api.github.com/users?client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    console.log(res.data);
+  //   this.setState({ users: res.data, isLoading: false});
+
+  //   console.log(res.data);
+  // }
+
+  searchUsers = async text => {
+    this.setState({ isLoading: true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, isLoading: false});
   }
 
   render() {
@@ -26,7 +36,7 @@ class App extends Component {
       <div className="App">
         <Navbar title='Github Finder'/>
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users isLoading={this.state.isLoading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Search = () => {
-    return (
-        <div>
-            <form className='form'>
-                <input type='text' name='text' placeholder='Search Users...' />
-                <input type='submit' value='Search' className='btn btn-dark btn-block'/>
-            </form>
-        </div>
-    )
+export class Search extends Component {
+    state = {
+        text: ''
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: '' });
+    };
+
+    // brackets allow you to use the input keys directly into the state hook
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit} className='form'>
+                    <input 
+                        type='text' 
+                        name='text' 
+                        placeholder='Search Users...' 
+                        value={this.state.text}
+                        onChange={this.onChange}
+                    />
+                    <input 
+                        type='submit' 
+                        value='Search' 
+                        className='btn btn-dark btn-block'
+                    />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Search
